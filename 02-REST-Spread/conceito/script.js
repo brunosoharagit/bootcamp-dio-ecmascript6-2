2f61143cb5bf2aee37f6d04258c03e7dd448be6c
brunosoharagit <b.sohara.git@gmail.com>
2020-05-25 12:29:06 -0300

    02 >> first commit

---
+++ 02-REST-Spread/conceito/script.js
@@ -0,0 +1,18 @@
+//rest operator ...
+function sum(...args)
+{
+    return args.reduce((acc, value) => acc + value, 0)
+}
+
+console.log(sum(5, 5, 2, 3, 3))
+
+
+const mul = (...args) => args.reduce((acc, value) => acc * value, 1)
+
+const sum2 = (a, b, ...rest) => {
+    return mul.apply(...rest)
+}
+
+console.log(5, 5, 2, 3, 3)
+
+//spread operator
